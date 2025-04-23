d4026fc3241e45e919b07cadf42c3c1bc271a9f9
Swapnanil1 <135691331+swapnanil1@users.noreply.github.com>
2025-04-23 15:54:05 +0530

    feat(sortProjects)#1: no two same name projects are allowed , also added fetching all tasks

--- src/handlers/projects/addProject.js
+++ src/handlers/projects/addProject.js
@@ -1,6 +1,16 @@
+import refreshProjects from "./getProjects";
+import refreshTasks from "./../tasks/getTasks";
 export default function drawProject(name) {
-  // lists the projects as a li and getProject hooks into this list to fetch updated projects
+  // lists the projects as a li and getProject hooks into this list to renders updated projects
   const projectNameVal = name.value.trim();
+  const currentProjectsList = refreshProjects().map((project) =>
+    project.toLowerCase()
+  );
+  // disallow repeated projects
+  if (currentProjectsList.includes(projectNameVal.toLowerCase())) {
+    alert("This project already exist");
+    return;
+  }
   const projectList = document.querySelector(".projects ul");
   const aProject = document.createElement("li");
   aProject.dataset.name = projectNameVal;
@@ -16,5 +26,8 @@ export default function drawProject(name) {
   delProjectBtn.textContent = "X";
   delProject.appendChild(delProjectBtn);
   aProject.appendChild(delProject);
+  // aProject.addEventListener("click", () => {
+  // todo logic for saving state of tasks
+  // });
   projectList.appendChild(aProject);
 }
