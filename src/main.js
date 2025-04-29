2b8c65ff6ca0eabed097c97de29e83e005b1b1bc
Swapnanil1 <135691331+swapnanil1@users.noreply.github.com>
2025-04-29 21:58:00 +0530

    working mobile sidebar btn on less viewport

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import "./styles/style.css";
 import addTaskMenu from "./handlers/tasks/addTaskMenu.js";
 import renderTasks from "./handlers/tasks/renderTasks.js";
-import {getAllTasks} from "./handlers/tasks/taskStorage.js";
+import { getAllTasks } from "./handlers/tasks/taskStorage.js";
 import setupModalListeners from "./handlers/tasks/editTask.js";
 import addProjectMenu from "./handlers/projects/addProjectMenu";
-import {renderAllProjects} from "./handlers/projects/renderProjects";
+import { renderAllProjects } from "./handlers/projects/renderProjects";
 import handleProjectEvents from "./handlers/projects/handleProjectEvents";
-
+import setupMobileMenu from "./handlers/setupMobileMenuToggle.js";
 addTaskMenu();
 setupModalListeners();
-addProjectMenu()
-renderAllProjects()
-handleProjectEvents()
-
+addProjectMenu();
+renderAllProjects();
+handleProjectEvents();
+setupMobileMenu();
 let allTask = getAllTasks();
 renderTasks(allTask);
 
